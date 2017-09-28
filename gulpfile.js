9d4142a5c9432adbc5beb1fc4de0cea490d3bfc3
MetaFM <fabrice.miras@gmail.com>
2017-09-28 23:33:57 +0200

    na

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function(){
+  console.log('hurray');
+})
+
+gulp.task('html', function(){
+  console.log('Imagine something usefull');
+})
