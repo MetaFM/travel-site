fd7830273a5c856891fd0776fda35252eafb2da6
MetaFM <fabrice.miras@gmail.com>
2017-09-29 20:01:09 +0200

    completed block

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,2 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-
-gulp.task('default', function(){
-  console.log('hurray');
-})
-
-gulp.task('html', function(){
-  console.log('Imagine something usefull');
-})
+require('./gulp/task/style');
+require('./gulp/task/watch');
